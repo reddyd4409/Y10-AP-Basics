efcdd30264d210eb7babd8150ffbd32f4792a80d
reddyd4409 <reddyd4409@masseyhigh.school.nz>
2025-09-16 12:15:35 +1200

    Variable, Basics

---
+++ P_02_Variables.py
@@ -0,0 +1,30 @@
+# Code to learn about variables...
+from os import times
+
+# Variables cam be a string; (Note the speech marks)
+greeting = "hatred towards: world"
+
+# Variables can be a number
+fav_num = 3
+num_string = "5"
+
+print(fav_num *2)
+print(num_string *5)
+
+
+# Constants are UPPERCASE
+PI = 3.14
+
+
+b_01_my_num = 7
+
+
+# Variables can be Boolean (True / False)
+HATE_programming = False
+
+print(greeting)
+print(fav_num)
+print(HATE_programming)
+
+# this variable has a problem?!
+my_name = "Miss 6"
